4c9822dfa67ebe7997814851a5a23fc3a8e66131
bar yerushalmian <bar.yerushalmian@gmail.com>
2022-03-09 15:34:10 +0200

    final version of q3 a and b complete!!

--- hm1/q3/q3.cpp
+++ hm1/q3/q3.cpp
@@ -5,21 +5,30 @@
 #include <cstring>
 using namespace std;
 
-char** StringBreakDown(char* str, int* size);
+char** StringBreakDown(char* str, int& size);
+char* DoubleSizeStr(char* Array);
 
 void main() {
 	//init variables
 	char* str;
-	int* size=NULL;
+	int size=0;
 	str = new char[16];
 	strcpy(str, "I hate homework");
 	char** newStr = StringBreakDown(str, size);
-
-	//error here also, size is null and does not have a value.this is for debug
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size; i++) //check for StringBreakDown
 	{
-		puts(newStr[i]);
-		cout << '\n' << endl;
+		cout << newStr[i] << endl;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		char* tmp= DoubleSizeStr(newStr[i]);
+		delete(newStr[i]);
+		newStr[i] = tmp;
+	}
+	
+	for (int i = 0; i < size; i++) //check for DoubleSizeStr
+	{
+		cout << newStr[i] << endl;
 		delete(newStr[i]);
 	}
 	delete(str);
@@ -27,8 +36,8 @@ void main() {
 }
 
 
-//Q3: The function gets a string and breaks it down to words. Returns a new array of strings.
-char** StringBreakDown(char* str, int* size)
+//Q3a: The function gets a string and breaks it down to words. Returns a new array of strings.
+char** StringBreakDown(char* str, int& size)
 {
 	//init variables
 	char** NewStr;
@@ -43,37 +52,65 @@ char** StringBreakDown(char* str, int* size)
 		{
 			newsize++;
 		}
+		
 	}
 
 	//allocating memory for the size
-	size = new int[newsize];
-	*size = newsize;
+	//size = new int[newsize];
+	size = newsize;
 
 	//allocating memory for the string
-	NewStr = new char*[(*size)];
-
+	NewStr = new char*[(size)];
 	int j, k;
 	j = k = 0;
+	//allocating memory to each pointer in the Array
+	for (int i = 0; 1 ; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\n'|| str[i] == NULL)
+		{
+			NewStr[k] = new char[(j + 1)];
+			k++;
+			j = 0;
+			if (str[i] == NULL) break;
+			continue;
+		}
+		j++;
+	}
 
-	/*The problem is here, I don't know if i copied it right.*/
+	
+	j = k = 0;
+
+	
 	//getting the size of each word, and allocating it accordingly.
-	for (int i=0; i < *size; j++)
+	for (int i=0; i < size;)
 	{
 
 		//End of the word
-		if (str[j] == ' ' || str[j] == '\n')
+		if (str[j] == ' ' || str[j] == '\n'||str[j] == NULL)
 		{
-			//Allocating memory
-			NewStr[i] = new char[j+1];
-
+			
 			//Copying the ammount of characters from the k'th spot, size of j.
-			strncpy(NewStr[i], &str[k], j);
-
+			int p = j - k;
+			strncpy(NewStr[i], &str[k], p);
+			NewStr[i][p] = '\0';
 			k = j+1;
 			i++;
+			if (str[j] == NULL) break;
 		}
+		j++;
 	}
 
 	return NewStr;
 
+}
+//Q3b: getting each sring of a word and double it, then return it
+char* DoubleSizeStr(char* Array)
+{
+	//getting size of string, then allocating tmp pointer, then double the string and copy it into newstr and return it
+		int strsize = strlen(Array);
+		char* newstr = new char[(2*strsize)+1];
+		strcpy(newstr, Array);
+		strcat(newstr, Array);
+		return newstr;
+		
 }
