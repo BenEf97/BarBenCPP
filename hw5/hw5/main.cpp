eeef2dea062a1f3a3d5d3aea688e0f33511d9608
BenEf97 <78364069+BenEf97@users.noreply.github.com>
2022-06-09 16:07:09 +0300

    init

---
+++ hw5/hw5/main.cpp
@@ -0,0 +1,9 @@
+class ClassName
+{
+private:
+	//Elements of our class, keep private from outside world.
+protected:
+	//Elements that are protected, but can be accsessed with inheritance.
+public:
+	//Usually methods and functions that can be used by the outside world.
+};
